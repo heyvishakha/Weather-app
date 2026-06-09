11de18acf0170f593e7c70b15616e0da1ed47a6f
heyvishakha <vishakhaburkul@gmail.com>
2026-06-09 23:59:54 +0530

    Refactor weather fetching and UI updates

--- script.js
+++ script.js
@@ -1,46 +1,82 @@
-function handleKeyPress(event) {
-if (event.key === "Enter") {
+const searchBtn = document.getElementById("searchBtn");
+const cityInput = document.getElementById("cityInput");
+
+searchBtn.addEventListener("click", getWeather);
+
+cityInput.addEventListener("keypress", function(event){
+if(event.key === "Enter"){
 getWeather();
 }
-}
+});
 
-async function getWeather() {
-const city = document.getElementById("cityInput").value.trim();
+async function getWeather(){
+
+const city = cityInput.value.trim();
 const result = document.getElementById("weatherResult");
 
-if (!city) {
-result.innerHTML = "<p>Enter a city name 😒</p>";
+if(city === ""){
+result.innerHTML = "<p>Enter city name 😒</p>";
 return;
 }
 
 result.innerHTML = "<p>Loading...</p>";
 
-try {
+const apiKey = "5913e31fdf174d4c50968a7b425a1395";
+
+try{
+
 const response = await fetch(
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5913e31fdf174d4c50968a7b425a1395&units=metric`
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 );
 
 const data = await response.json();
 
-if (data.cod != 200) {
-  result.innerHTML = "<p>City not found ❌</p>";
-  return;
+if(data.cod != 200){
+result.innerHTML = "<p>City not found ❌</p>";
+return;
 }
 
 result.innerHTML = `
-  <h2>${data.name}</h2>
-  <h1>${Math.round(data.main.temp)}°C</h1>
-  <p>${data.weather[0].main}</p>
-  <p>Humidity: ${data.main.humidity}%</p>
-  <p>Wind: ${data.wind.speed} m/s</p>
-  <p>Feels Like: ${Math.round(data.main.feels_like)}°C</p>
-  <p>Pressure: ${data.main.pressure} hPa</p>
+<div class="weather-card">
+
+<h2>${data.name}</h2>
+
+<h1>${Math.round(data.main.temp)}°C</h1>
+
+<p>${data.weather[0].main}</p>
+
+<div class="details">
+
+<div class="detail-card">
+<h4>💧 Humidity</h4>
+<p>${data.main.humidity}%</p>
+</div>
+
+<div class="detail-card">
+<h4>🌬 Wind</h4>
+<p>${data.wind.speed} m/s</p>
+</div>
+
+<div class="detail-card">
+<h4>🌡 Feels Like</h4>
+<p>${Math.round(data.main.feels_like)}°C</p>
+</div>
+
+<div class="detail-card">
+<h4>📈 Pressure</h4>
+<p>${data.main.pressure} hPa</p>
+</div>
+
+</div>
+
+</div>
 `;
 
-
-} catch (error) {
+}
+catch(error){
 result.innerHTML = "<p>Error loading weather ❌</p>";
 console.log(error);
 }
+
 }
 
